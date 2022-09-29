767d786767dced64d9eb9dcef3b1fcb60cfea1f9
matemaster <matemaster@163.com>
2022-09-29 20:50:56 +0800

    #feat json序列化工具

---
+++ ibee-demo/ibee-demo-json/src/main/java/cs/matemaster/demo/JsonUtil.java
@@ -0,0 +1,65 @@
+package cs.matemaster.demo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author matemaster
+ */
+public final class JsonUtil {
+
+    private static final ObjectMapper Mapper = new ObjectMapper();
+
+    public static String serialize(Object arg) {
+        if (arg == null) {
+            return null;
+        }
+        try {
+            return Mapper.writeValueAsString(arg);
+        } catch (JsonProcessingException ignore) {
+            return null;
+        }
+    }
+
+    public static String serializeIgnoreNull(Object arg) {
+        if (arg == null) {
+            return null;
+        }
+        try {
+            Mapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
+            return Mapper.writeValueAsString(arg);
+        } catch (JsonProcessingException ignore) {
+            return null;
+        }
+    }
+
+    public static <T> T deserialize(String jsonStr, Class<T> type) {
+        if (jsonStr == null) {
+            return null;
+        }
+
+        try {
+            Mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            return Mapper.readValue(jsonStr, type);
+        } catch (JsonProcessingException ignore) {
+            return null;
+        }
+    }
+
+    public static <T> T deserialize(String jsonStr, TypeReference<T> typeReference) {
+        if (jsonStr == null) {
+            return null;
+        }
+
+        try {
+            Mapper.disable(JsonGenerator.Feature.IGNORE_UNKNOWN);
+            return Mapper.readValue(jsonStr, typeReference);
+        } catch (JsonProcessingException ignore) {
+            return null;
+        }
+    }
+}
