15d43f00a395132bf3b36c6ee04b80e2cdd0fc7d
matemaster <matemaster@163.com>
2022-10-01 13:40:32 +0800

    #feat Jackson处理序列化与反序列化

--- ibee-jackson/src/main/java/cs/matemaster/jackson/JacksonUtil.java
+++ ibee-jackson/src/main/java/cs/matemaster/jackson/JacksonUtil.java
@@ -1,6 +1,9 @@
 package cs.matemaster.jackson;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -16,6 +19,33 @@ public final class JacksonUtil {
     public static String serialize(Object arg) {
         try {
             return OBJECT_MAPPER.writeValueAsString(arg);
+        } catch (JsonProcessingException ignore) {
+            return null;
+        }
+    }
+
+    public static String serializeIgnoreNull(Object arg) {
+        try {
+            OBJECT_MAPPER.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
+            return OBJECT_MAPPER.writeValueAsString(arg);
+        } catch (JsonProcessingException ignore) {
+            return null;
+        }
+    }
+
+    public static <T> T deserialize(String jsonStr, Class<T> targetType) {
+        try {
+            OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            return OBJECT_MAPPER.readValue(jsonStr, targetType);
+        } catch (JsonProcessingException ignore) {
+            return null;
+        }
+    }
+
+    public static <T> T deserialize(String jsonStr, TypeReference<T> typeReference) {
+        try {
+            OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            return OBJECT_MAPPER.readValue(jsonStr, typeReference);
         } catch (JsonProcessingException ignore) {
             return null;
         }
