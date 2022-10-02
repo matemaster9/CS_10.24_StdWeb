318ee3f2e63a17855088532b8bf895bae8c434e8
matemaster <matemaster@163.com>
2022-10-02 11:34:56 +0800

    #test 构造器绑定配置

---
+++ ibee-demo/ibee-demo-config/src/main/java/cs/matemaster/demo/config/externalize/MultiConstructorBingingConfig.java
@@ -0,0 +1,56 @@
+package cs.matemaster.demo.config.externalize;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import java.util.List;
+
+/**
+ * @author matemaster
+ */
+@ConfigurationProperties(prefix = "multi-constructor")
+@EnableConfigurationProperties(MultiConstructorBingingConfig.class)
+public class MultiConstructorBingingConfig {
+
+    private final String username;
+
+    private final String password;
+
+    private final List<String> inetAddressList;
+
+    public MultiConstructorBingingConfig() {
+        username = null;
+        password = null;
+        inetAddressList = null;
+    }
+
+    @ConstructorBinding
+    public MultiConstructorBingingConfig(String username, String password, @DefaultValue List<String> inetAddressList) {
+        this.username = username;
+        this.password = password;
+        this.inetAddressList = inetAddressList;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<String> getInetAddressList() {
+        return inetAddressList;
+    }
+
+    @Override
+    public String toString() {
+        return "MultiConstructorBingingConfig{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", inetAddressList=" + inetAddressList +
+                '}';
+    }
+}
