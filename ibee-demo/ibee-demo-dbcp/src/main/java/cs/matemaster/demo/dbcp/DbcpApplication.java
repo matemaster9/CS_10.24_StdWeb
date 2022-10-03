94bc9766e264472e6b67e63dc41bda1b607ae9a1
matemaster <matemaster@163.com>
2022-10-03 11:30:45 +0800

    #feat swagger编写接口

--- ibee-demo/ibee-demo-dbcp/src/main/java/cs/matemaster/demo/dbcp/DbcpApplication.java
+++ ibee-demo/ibee-demo-dbcp/src/main/java/cs/matemaster/demo/dbcp/DbcpApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author matemaster
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "cs.matemaster.demo.dbcp",
+        "cs.matemaster.swagger"
+})
 public class DbcpApplication {
     public static void main(String[] args) {
         SpringApplication.run(DbcpApplication.class, args);
