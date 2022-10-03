532b1f4c897e42c550b334998e9eb37ec431d3c5
matemaster <matemaster@163.com>
2022-10-03 16:11:40 +0800

    #chore 引入统一接口

--- ibee-demo/ibee-demo-dbcp/src/main/java/cs/matemaster/demo/dbcp/DbcpApplication.java
+++ ibee-demo/ibee-demo-dbcp/src/main/java/cs/matemaster/demo/dbcp/DbcpApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(scanBasePackages = {
         "cs.matemaster.demo.dbcp",
-        "cs.matemaster.swagger"
+        "cs.matemaster.swagger",
+        "cs.matemaster.global"
 })
 public class DbcpApplication {
     public static void main(String[] args) {
