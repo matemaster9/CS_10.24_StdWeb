bffa24c2245073b60f9bbde031c6aa699e6caddc
matemaster <matemaster@163.com>
2022-10-03 08:29:39 +0800

    #chore 新建dbcp模块

---
+++ ibee-demo/ibee-demo-dbcp/src/main/java/cs/matemaster/demo/dbcp/DbcpApplication.java
@@ -0,0 +1,14 @@
+package cs.matemaster.demo.dbcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author matemaster
+ */
+@SpringBootApplication
+public class DbcpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbcpApplication.class, args);
+    }
+}
