f15789e576cf1c174fbcd805679efee977f19331
matemaster <matemaster@163.com>
2022-10-15 22:22:56 +0800

    #chore JedisApp项目构建

---
+++ ibee-demo/ibee-demo-jedis/src/main/java/cs/matemaster/demo/jedis/JedisApplication.java
@@ -0,0 +1,13 @@
+package cs.matemaster.demo.jedis;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author matemaster
+ */
+@SpringBootApplication
+public class JedisApplication {
+    public static void main(String[] args) {
+
+    }
+}
