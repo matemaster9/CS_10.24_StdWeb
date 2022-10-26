f9db5294319728adfed15c6c17c42ae347f5ba1a
matemaster <matemaster@163.com>
2022-10-27 00:15:30 +0800

    #style config优化

--- ibee-tech/ibee-tech-es8/src/main/java/cs/matemaster/tech/es8/config/ElasticConfig.java
+++ ibee-tech/ibee-tech-es8/src/main/java/cs/matemaster/tech/es8/config/ElasticConfig.java
@@ -12,6 +12,7 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.ssl.SSLContexts;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,28 +32,34 @@ public class ElasticConfig {
     @Bean
     public ElasticsearchClient elasticsearchClient() {
 
-        RestClient restClient = RestClient.builder(new HttpHost(elasticYml.getAddress(), elasticYml.getPort(), elasticYml.getScheme()))
-                .setHttpClientConfigCallback(httpAsyncClientBuilder -> {
-
-                    // 配置es 用户密码认证
-                    final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-                    credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(elasticYml.getUsername(), elasticYml.getPassword()));
-                    httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-
-                    // 配置es ssl访问
-                    final SSLContext sslContext;
-                    try {
-                        // 信任所有
-                        sslContext = SSLContexts.custom().loadTrustMaterial(null, (x509Certificates, s) -> true).build();
-                    } catch (Exception e) {
-                        log.debug(e.getMessage());
-                        return null;
-                    }
-                    httpAsyncClientBuilder.setSSLContext(sslContext);
-
-                    return httpAsyncClientBuilder;
-                }).build();
+        RestClient restClient = RestClient
+                .builder(new HttpHost(elasticYml.getAddress(), elasticYml.getPort(), elasticYml.getScheme()))
+                .setHttpClientConfigCallback(getHttpClientConfigCallback())
+                .build();
         RestClientTransport restClientTransport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         return new ElasticsearchClient(restClientTransport);
+    }
+
+    public RestClientBuilder.HttpClientConfigCallback getHttpClientConfigCallback() {
+
+        return httpAsyncClientBuilder -> {
+
+            // 配置es 用户密码认证
+            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(elasticYml.getUsername(), elasticYml.getPassword()));
+            httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+
+            // 配置es ssl访问
+            final SSLContext sslContext;
+            try {
+                // 信任所有
+                sslContext = SSLContexts.custom().loadTrustMaterial(null, (x509Certificates, s) -> true).build();
+            } catch (Exception e) {
+                log.debug(e.getMessage());
+                return null;
+            }
+            httpAsyncClientBuilder.setSSLContext(sslContext);
+            return httpAsyncClientBuilder;
+        };
     }
 }
