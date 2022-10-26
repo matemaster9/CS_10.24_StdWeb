bb54583b4642a5f83f9ad023b5f52d2480755106
matemaster <matemaster@163.com>
2022-10-27 00:05:57 +0800

    #test match查询测试

--- ibee-tech/ibee-tech-es8/src/test/java/ElasticApiTest.java
+++ ibee-tech/ibee-tech-es8/src/test/java/ElasticApiTest.java
@@ -1,4 +1,8 @@
+import co.elastic.clients.elasticsearch._types.query_dsl.MatchQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch.core.GetRequest;
 import co.elastic.clients.elasticsearch.core.IndexRequest;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
 import cs.matemaster.tech.es8.model.StaffWorkLogDto;
 import org.junit.Test;
 
@@ -24,6 +28,36 @@ public class ElasticApiTest {
                 .document(workLog)
         );
 
+        System.out.println(request);
+    }
+
+    @Test
+    public void test2() {
+        MatchQuery matchQuery = MatchQuery.of(builder -> builder
+                .field("workingHours")
+                .query(8)
+        );
+        System.out.println(matchQuery);
+
+
+        Query query = Query.of(builder -> builder.match(matchQuery));
+        System.out.println(query);
+
+        SearchRequest searchRequest = SearchRequest.of(builder -> builder
+                .index("work-log")
+                .query(query)
+        );
+        System.out.println(searchRequest);
+    }
+
+    @Test
+    public void test3() {
+        GetRequest request = GetRequest.of(builder -> builder
+                .index("work-log")
+                .id("2022-10-26")
+        );
+
+        // GET /work-log/_doc/2022-10-26
         System.out.println(request);
     }
 }
