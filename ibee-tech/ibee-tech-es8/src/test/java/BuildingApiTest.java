19f25aa01caf3e0616f4c2524fb7cb1e1a2603dc
matemaster <matemaster@163.com>
2022-10-26 21:34:52 +0800

    #feat es building api objects特性学习1

--- ibee-tech/ibee-tech-es8/src/test/java/BuildingApiTest.java
+++ ibee-tech/ibee-tech-es8/src/test/java/BuildingApiTest.java
@@ -17,5 +17,14 @@ public class BuildingApiTest {
         );
 
         System.out.println(developer);
+
+        Developer<SystemManager> developer_v1 = new Developer.Builder<SystemManager>()
+                .name("MateMaster")
+                .age(24)
+                .job("系统开发")
+                .project(new SystemManager("后台管理系统范例", "后台通用功能"))
+                .build();
+
+        System.out.println(developer_v1);
     }
 }
