4bf6e6acf5b0f09b88cb33682a734b1101c4528b
matemaster <matemaster@163.com>
2022-10-26 20:38:59 +0800

    #test 新增文档示例

--- ibee-tech/ibee-tech-es8/src/test/java/ElasticApiTest.java
+++ ibee-tech/ibee-tech-es8/src/test/java/ElasticApiTest.java
@@ -1,4 +1,8 @@
+import co.elastic.clients.elasticsearch.core.IndexRequest;
+import cs.matemaster.tech.es8.model.StaffWorkLogDto;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
 
 /**
  * @author matemaster
@@ -7,6 +11,19 @@ public class ElasticApiTest {
 
     @Test
     public void test1() {
+        StaffWorkLogDto workLog = new StaffWorkLogDto();
+        workLog.setWorkingHours(8);
+        workLog.setStaffCode("80327878");
+        workLog.setOnDuty(LocalDateTime.now().minusHours(8));
+        workLog.setOffDuty(LocalDateTime.now());
 
+
+        IndexRequest<StaffWorkLogDto> request = IndexRequest.of(builder -> builder
+                .index("work-log")
+                .id("2022-10-26")
+                .document(workLog)
+        );
+
+        System.out.println(request);
     }
 }
