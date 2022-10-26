8e97ed9cd3662254c019678d4ac98fe6e839b9a5
matemaster <matemaster@163.com>
2022-10-26 20:45:23 +0800

    #style 索引话术

---
+++ ibee-tech/ibee-tech-es8/src/main/java/cs/matemaster/tech/es8/config/ElasticConstant.java
@@ -0,0 +1,13 @@
+package cs.matemaster.tech.es8.config;
+
+/**
+ * ES 索引
+ *
+ * @author matemaster
+ */
+public interface ElasticConstant {
+
+    String SystemUserIndex = "systemuser";
+
+    String StaffWorkLogIndex = "staffworklog";
+}
