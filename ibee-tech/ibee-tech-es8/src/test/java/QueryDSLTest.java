8395735a36eef3189cf9af971448b19e9c8d12c1
matemaster <matemaster@163.com>
2022-10-27 21:15:42 +0800

    #feat terms查询

--- ibee-tech/ibee-tech-es8/src/test/java/QueryDSLTest.java
+++ ibee-tech/ibee-tech-es8/src/test/java/QueryDSLTest.java
@@ -1,8 +1,15 @@
+import co.elastic.clients.elasticsearch._types.FieldValue;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
 import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermsQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermsQueryField;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author matemaster
@@ -34,7 +41,23 @@ public class QueryDSLTest {
 
     @Test
     public void terms() {
+        List<String> values = Arrays.asList("bvw32iu388", "v2hg234", "9w28e4h9uv");
+        List<FieldValue> fieldValues = values.stream().map(FieldValue::of).collect(Collectors.toList());
+        TermsQueryField termsQueryField = TermsQueryField.of(builder1 -> builder1.value(fieldValues));
+        TermsQuery termsQuery = TermsQuery.of(builder -> builder.field("_id").terms(termsQueryField));
 
+        Query query = Query.of(builder -> builder.terms(termsQuery));
+        Query terms = QueryBuilders.terms(builder -> builder.field("_id").terms(termsQueryField));
+
+        System.out.println(termsQuery);
+        System.out.println(query);
+        System.out.println(terms);
+
+        SearchRequest searchRequest = SearchRequest.of(builder -> builder
+                .index("systemuser")
+                .query(terms)
+        );
+        System.out.println(searchRequest);
     }
 
     @Test
